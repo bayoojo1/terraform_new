3c500ccb8ca4b00e38a01edc03423e31cc665bd6
Ojo Adebayo <bayoojo1@yahoo.com>
2024-07-09 23:24:28 +0100

    adding latest commit

--- 06-resources/compute.tf
+++ 06-resources/compute.tf
@@ -1,42 +1,42 @@
-resource "aws_instance" "web" {
-  ami                         = "ami-03f7e9012611e34eb"
-  associate_public_ip_address = true
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.public_http_traffic.id]
-  root_block_device {
-    delete_on_termination = true
-    volume_size           = 10
-    volume_type           = "gp3"
-  }
+# resource "aws_instance" "web" {
+#   ami                         = "ami-03f7e9012611e34eb"
+#   associate_public_ip_address = true
+#   instance_type               = "t2.micro"
+#   subnet_id                   = aws_subnet.public.id
+#   vpc_security_group_ids      = [aws_security_group.public_http_traffic.id]
+#   root_block_device {
+#     delete_on_termination = true
+#     volume_size           = 10
+#     volume_type           = "gp3"
+#   }
 
-  tags = merge(local.common_tags, {
-    Name = "06-resources-ec2"
-  })
-}
+#   tags = merge(local.common_tags, {
+#     Name = "06-resources-ec2"
+#   })
+# }
 
-resource "aws_security_group" "public_http_traffic" {
-  description = "Security group allowing traffic on port 443 and 80"
-  name        = "public-http-traffic"
-  vpc_id      = aws_vpc.main.id
+# resource "aws_security_group" "public_http_traffic" {
+#   description = "Security group allowing traffic on port 443 and 80"
+#   name        = "public-http-traffic"
+#   vpc_id      = aws_vpc.main.id
 
-  tags = merge(local.common_tags, {
-    Name = "06-resources-sg"
-  })
-}
+#   tags = merge(local.common_tags, {
+#     Name = "06-resources-sg"
+#   })
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "http" {
-  security_group_id = aws_security_group.public_http_traffic.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "tcp"
-  from_port         = 80
-  to_port           = 80
-}
+# resource "aws_vpc_security_group_ingress_rule" "http" {
+#   security_group_id = aws_security_group.public_http_traffic.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "tcp"
+#   from_port         = 80
+#   to_port           = 80
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "https" {
-  security_group_id = aws_security_group.public_http_traffic.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "tcp"
-  from_port         = 443
-  to_port           = 443
-}
+# resource "aws_vpc_security_group_ingress_rule" "https" {
+#   security_group_id = aws_security_group.public_http_traffic.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "tcp"
+#   from_port         = 443
+#   to_port           = 443
+# }
